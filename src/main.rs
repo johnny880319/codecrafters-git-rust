c4f07bff0667d1c3c77ea9c071950ac65f5b8319
Johnny_Ma <johnny880319@gmail.com>
2026-03-23 02:43:39 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,30 @@ use std::env;
 use std::fs;
 use std::io::Read;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
 
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+        fs::create_dir(".git")?;
+        fs::create_dir(".git/objects")?;
+        fs::create_dir(".git/refs")?;
+        fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
         println!("Initialized git directory");
     } else if args[1] == "cat-file" {
         if args[2] != "-p" {
             eprintln!("Usage: cat-file -p <object>");
-            return;
+            return Ok(());
         }
         let object_hash = &args[3];
         let object_path = format!(".git/objects/{}/{}", &object_hash[0..2], &object_hash[2..]);
-        let mut decoder = GzDecoder::new(fs::File::open(object_path).unwrap());
+        let mut decoder = GzDecoder::new(fs::File::open(object_path)?);
         let mut contents = String::new();
-        decoder.read_to_string(&mut contents).unwrap();
+        decoder.read_to_string(&mut contents)?;
         println!("{contents}");
     } else {
         println!("unknown command: {}", args[1]);
     }
+    Ok(())
 }
