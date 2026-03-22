f978177d94cdf0e501ea2ffbe205e0d5d8ae22bb
Johnny_Ma <johnny880319@gmail.com>
2026-03-23 02:34:34 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use flate2::read::GzDecoder;
 use std::env;
 use std::fs;
+use std::io::Read;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,6 +14,17 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory");
+    } else if args[1] == "cat-file" {
+        if args[2] != "-p" {
+            eprintln!("Usage: cat-file -p <object>");
+            return;
+        }
+        let object_hash = &args[3];
+        let object_path = format!(".git/objects/{}/{}", &object_hash[0..2], &object_hash[2..]);
+        let mut decoder = GzDecoder::new(fs::File::open(object_path).unwrap());
+        let mut contents = String::new();
+        decoder.read_to_string(&mut contents).unwrap();
+        println!(contents);
     } else {
         println!("unknown command: {}", args[1]);
     }
